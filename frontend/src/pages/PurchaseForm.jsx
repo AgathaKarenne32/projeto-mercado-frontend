2b62313d42afbd50d6f50f075c0dcd7a1878e43f
leonamsh <lpdmonteiro@gmail.com>
2025-08-14 20:36:16 -0300

    add: Puchase Form e List da branch main

---
+++ frontend/src/pages/PurchaseForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PurchaseForm = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
+
+export default PurchaseForm;
---
+++ frontend/src/pages/PurchaseList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StatCard from "../components/StatCard";
+
+const PurchaseList = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
+
+export default PurchaseList;
