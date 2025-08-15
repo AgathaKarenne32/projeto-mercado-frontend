40612e83d709cc4f4c0f0bbea3e7b71ffa1ff9d6
leonamsh <lpdmonteiro@gmail.com>
2025-08-14 22:00:18 -0300

    fix: correção do diretório e import dos PurchaseForm e PurchaseList

--- frontend/src/components/PurchaseForm.jsx
+++ frontend/src/components/PurchaseForm.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const PurchaseForm = () => {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
+  return <div>Hello World</div>;
 };
 
 export default PurchaseForm;
