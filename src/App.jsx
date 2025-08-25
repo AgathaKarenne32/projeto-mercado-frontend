7b5ecbfe663520c826e081fbb5f2b8233c17cbf7
ca-madureira <ca.madureira094@gmail.com>
2025-08-25 20:14:18 -0300

    alter import header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/Header-Mobile/header-mobile";
 
 
 const App = () => {
