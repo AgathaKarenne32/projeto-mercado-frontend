4194fc5353ebdf1d5073ffd2df3bc0ba4f42475c
ca-madureira <ca.madureira094@gmail.com>
2025-09-07 17:32:52 -0300

    feat: add api integration with axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // <-- importe aqui
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ToastContainer />
       <AppRoutes />
     </BrowserRouter>
   </StrictMode>,
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,39 @@
 import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { api } from '../services/api';
+import { toast } from 'react-toastify'
 
-const SignUp = ({ onSignUpSuccess, onLoginClick }) => {
+const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors }
+    } = useForm();
+
+    const password = watch("password");
+
+    const saveUser = async (userData) => {
+        try {
+            await api.post("/auth/register", userData);
+            reset();
+            toast.success("Usuário criado com sucesso!")
+        } catch (err) {
+            toast.error("Erro ao cadastrar usuario")
+            console.log("Erro ao cadastrar")
+        }
+
+    };
+
+    const onSubmit = (data) => {
+        const { confirmPassword, ...userData } = data;
+        saveUser(userData);
+    };
+
     return (
         <section id="signup-screen" className="screen active phone-mockup">
-            <div className="main-content">
+            <form className="main-content" onSubmit={handleSubmit(onSubmit)}>
                 <div className="login-header">
                     <div className="login-logo">
                         <i className="fas fa-user-plus"></i>
@@ -13,59 +43,64 @@ const SignUp = ({ onSignUpSuccess, onLoginClick }) => {
                 </div>
 
                 <div className="form-container">
-                    <label htmlFor="signup-name" className="form-label">
-                        Nome completo
-                    </label>
+                    <label htmlFor="signup-name" className="form-label">Nome completo</label>
                     <input
                         type="text"
                         id="signup-name"
                         placeholder="Digite seu nome completo"
+                        {...register("username", { required: "Nome é obrigatório" })}
                         className="form-input"
                     />
+                    {errors.name && <span>{errors.name.message}</span>}
                 </div>
 
                 <div className="form-container">
-                    <label htmlFor="signup-email" className="form-label">
-                        E-mail
-                    </label>
+                    <label htmlFor="signup-email" className="form-label">E-mail</label>
                     <input
                         type="email"
                         id="signup-email"
                         placeholder="Digite seu e-mail"
                         className="form-input"
+                        {...register("email", {
+                            required: "Email é obrigatório",
+                            pattern: {
+                                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                                message: "Email inválido"
+                            }
+                        })}
                     />
+                    {errors.email && <span>{errors.email.message}</span>}
                 </div>
 
                 <div className="form-container">
-                    <label htmlFor="signup-password" className="form-label">
-                        Senha
-                    </label>
+                    <label htmlFor="signup-password" className="form-label">Senha</label>
                     <input
                         type="password"
                         id="signup-password"
                         placeholder="Mínimo 8 caracteres"
                         className="form-input"
+                        {...register("password", { required: "Senha obrigatória" })}
                     />
+                    {errors.password && <span>{errors.password.message}</span>}
                 </div>
 
                 <div className="form-container">
-                    <label htmlFor="signup-confirm-password" className="form-label">
-                        Confirmar senha
-                    </label>
+                    <label htmlFor="signup-confirm-password" className="form-label">Confirmar senha</label>
                     <input
                         type="password"
                         id="signup-confirm-password"
                         placeholder="Repita sua senha"
                         className="form-input"
+                        {...register("confirmPassword", {
+                            required: "Confirmação obrigatória",
+                            validate: value => value === password || "As senhas não coincidem"
+                        })}
                     />
+                    {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
                 </div>
 
                 <div className="form-container">
-                    <button
-                        id="create-account-button"
-                        className="main-button"
-                        onClick={onSignUpSuccess}
-                    >
+                    <button type="submit" id="create-account-button" className="main-button">
                         <i className="fas fa-user-plus"></i>
                         Criar conta
                     </button>
@@ -73,13 +108,7 @@ const SignUp = ({ onSignUpSuccess, onLoginClick }) => {
 
                 <div className="signup-text">
                     <span className="signup-label">Já tem uma conta?</span>
-                    <Link
-                        to="/login"
-                        id="login-link"
-                        className="signup-link"
-                    >
-                        Fazer login
-                    </Link>
+                    <Link to="/login" id="login-link" className="signup-link">Fazer login</Link>
                 </div>
 
                 <div className="features-list">
@@ -98,7 +127,7 @@ const SignUp = ({ onSignUpSuccess, onLoginClick }) => {
                         </li>
                     </ul>
                 </div>
-            </div>
+            </form>
         </section>
     );
 };
